9b8dfb61b860e886e35bb080996d765b318b9a33
Igor Sharapov <igorsharapov@outlook.com>
2024-12-15 14:45:03 +0200

    Hello world QML app

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
-int main () {
-  return 0;
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main (int argc, char** argv) {
+  QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+
+  const QUrl mainPage {u"qrc:/qt/qml/long_do/main.qml"_qs};
+  QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+		   &app, [&mainPage](QObject *obj, const QUrl &objUrl) {
+		     if (!obj && mainPage == objUrl)
+		       QCoreApplication::exit(-1);
+		   }, Qt::QueuedConnection);
+  engine.load(mainPage);
+
+  return app.exec();
 }
