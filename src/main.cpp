3b1f6446d0f2acadbd207a1f9b0af62af9dea4f1
Igor Sharapov <igorsharapov@outlook.com>
2024-12-17 13:34:11 +0200

    Add clang-tidy check (#3)
    * Add clang-tidy check
    
    * Add llvm installation
    
    * Try clang 18
    
    * Install llvm from apt
    
    * Use preinstalled llvm 18
    
    * Add clang-tidy file
    
    * Debug clang-tidy
    
    * Regenerate config file
    
    * Remove debug steps

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
+#include <QCoreApplication>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QObject>
+#include <QString>
+#include <Qt>
+#include <QUrl>
 
 int main (int argc, char** argv) {
   QGuiApplication app(argc, argv);
@@ -9,10 +14,11 @@ int main (int argc, char** argv) {
   const QUrl mainPage {u"qrc:/qt/qml/long_do/main.qml"_qs};
   QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
 		   &app, [&mainPage](QObject *obj, const QUrl &objUrl) {
-		     if (!obj && mainPage == objUrl)
+		     if (obj != nullptr && mainPage == objUrl) {
 		       QCoreApplication::exit(-1);
+		     }
 		   }, Qt::QueuedConnection);
   engine.load(mainPage);
 
-  return app.exec();
+  return QGuiApplication::exec();
 }
