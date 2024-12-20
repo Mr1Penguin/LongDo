f80b7c4d0b98b92349092c07f44e3326619d6e94
Igor Sharapov <igorsharapov@outlook.com>
2024-12-20 11:07:27 +0200

    clang-format main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,26 @@
 #include <QCoreApplication>
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
 #include <QObject>
+#include <QQmlApplicationEngine>
 #include <QString>
-#include <Qt>
 #include <QUrl>
+#include <Qt>
 
-int main (int argc, char** argv) {
+int main(int argc, char** argv) {
+
   QGuiApplication app(argc, argv);
 
   QQmlApplicationEngine engine;
 
-  const QUrl mainPage {u"qrc:/qt/qml/long_do/main.qml"_qs};
-  QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-		   &app, [&mainPage](QObject *obj, const QUrl &objUrl) {
-		     if (obj != nullptr && mainPage == objUrl) {
-		       QCoreApplication::exit(-1);
-		     }
-		   }, Qt::QueuedConnection);
+  const QUrl mainPage{u"qrc:/qt/qml/long_do/main.qml"_qs};
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [&mainPage](QObject* obj, const QUrl& objUrl) {
+        if (obj != nullptr && mainPage == objUrl) {
+          QCoreApplication::exit(-1);
+        }
+      },
+      Qt::QueuedConnection);
   engine.load(mainPage);
 
   return QGuiApplication::exec();
