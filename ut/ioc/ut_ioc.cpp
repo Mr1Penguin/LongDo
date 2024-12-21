2081a8f4ebd81bc8cb2c304241faff9b552a58f6
Igor Sharapov <igorsharapov@outlook.com>
2024-12-21 18:51:47 +0200

    Add ut-clang-tidy and flags for ut

--- ut/ioc/ut_ioc.cpp
+++ ut/ioc/ut_ioc.cpp
@@ -5,31 +5,44 @@
 
 #include "Ioc.hpp"
 
+namespace {
+
 class Interface {
 public:
-  virtual ~Interface() = default;  
+  Interface()                            = default;
+  Interface(const Interface&)            = default;
+  Interface(Interface&&)                 = delete;
+  Interface& operator=(const Interface&) = default;
+  Interface& operator=(Interface&&)      = delete;
+  virtual ~Interface()                   = default;
 };
+
 class Implementation : public Interface {};
+
+} // namespace
 
 class IocTest : public QObject {
   Q_OBJECT
 
 private slots:
+  // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
   void shouldReturnNullptrForUnregistered() {
-    long_do::ioc::Ioc ioc;
+    const long_do::ioc::Ioc ioc;
     const std::shared_ptr<Interface> serviceInterface = ioc.resolveService<Interface>();
     QVERIFY(!serviceInterface);
   }
-  
+
+  // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
   void shouldResolveRegisteredServiceByInterace() {
     long_do::ioc::Ioc ioc;
     ioc.registerService<Interface, Implementation>();
     const std::shared_ptr<Interface> serviceInterface = ioc.resolveService<Interface>();
     QVERIFY(serviceInterface);
-    const std::shared_ptr<Implementation> serviceImplementation = std::dynamic_pointer_cast<Implementation>(serviceInterface);
+    const std::shared_ptr<Implementation> serviceImplementation =
+        std::dynamic_pointer_cast<Implementation>(serviceInterface);
     QVERIFY(serviceImplementation);
   }
 };
 
-QTEST_MAIN(IocTest);
+QTEST_MAIN(IocTest)
 #include "ut_ioc.moc"
